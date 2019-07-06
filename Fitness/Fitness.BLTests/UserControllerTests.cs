7d0c12f1567d79d36f41518beab87aab4b602f0c
romantulchak <romantylchak29@gmail.com>
2019-07-06 20:31:27 +0300

    v1.2.2

---
+++ Fitness/Fitness.BLTests/UserControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fitness.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+       
+
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            var userName = Guid.NewGuid().ToString();
+            var birthdate = DateTime.Now.AddYears(-18);
+            var gender = "man";
+            var weight = 90;
+            var height = 190;
+
+            var controller = new UserController(userName);
+            controller.SetNewUserData(gender, birthdate, weight, height);
+            var controller2 = new UserController(userName);
+
+            Assert.AreEqual(userName, controller2.CurrentUser.Name);
+            Assert.AreEqual(birthdate, controller2.CurrentUser.BirthDate);
+            Assert.AreEqual(gender, controller2.CurrentUser.Gender.Name);
+            Assert.AreEqual(weight, controller2.CurrentUser.Weight);
+            Assert.AreEqual(height, controller2.CurrentUser.Height);
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            var userName = Guid.NewGuid().ToString();
+            var controller = new UserController(userName);
+
+
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+    }
+}
---
+++ Fitness/Fitness.TEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fitness.TEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fitness.TEST")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f47dae79-18b1-496e-9ac2-0ce0af21f5b1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
