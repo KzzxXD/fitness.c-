6e017c364547c2736b77ff994d7a4a587ac6589d
romantulchak <romantylchak29@gmail.com>
2019-07-06 11:26:00 +0300

    v1.1.2

---
+++ Fitness/Fitness.BL/Model/Gender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    ///Стать  
+    ///
+    /// </summary>
+    public class Gender
+    {/// <summary>
+    /// Назва
+    /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Створити нову стать
+        /// </summary>
+        /// <param name="name"></param>
+        public Gender(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Ім'я статі не може бути пустим або null", nameof(name));
+            }
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Fitness/Fitness.BL/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Model
+{
+    public class User
+    {
+        public string Name { get; }
+
+        public Gender Gender { get; }
+
+        public DateTime BirthDate { get; }
+
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+
+    }
+}
